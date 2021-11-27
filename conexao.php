000a2c4cd7f5194e12086044db0c0137a27603d5
Juscelino Campos <celino3x@gmail.com>
2021-11-27 11:36:30 -0300

    update data

--- conexao.php
+++ conexao.php
@@ -1,9 +1,11 @@
 <?php
-  $servename = "10.19.100.6";
-  $database = "estoque";
-  $username = "celino3x";
-  $password = "Jw1914JC";
+  $servename = "us-cdbr-east-04.cleardb.com";
+  $database = "heroku_fb572d9119229b8";
+  $username = "bdf6b07da17311";
+  $password = "421addf8";
 
   $conexao = mysqli_connect($servename, $username, $password, $database);
 
 ?>
+
+mysql://bdf6b07da17311:421addf8@us-cdbr-east-04.cleardb.com/heroku_fb572d9119229b8?reconnect=true
